46f7b7e66799983e3af009b286c0396ac835ab6b
FelixAMX <35340933+FelixAMX@users.noreply.github.com>
2021-04-30 13:00:17 +0500

    new slick-slider new popup-gallery

---
+++ js/slick-slider.js
@@ -0,0 +1,89 @@
+$(function(){
+    $('.sl-slider').slick({
+        arrows:true,
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        adaptiveHeight: true,
+        waitForAnimate: true,
+        //fade: true,
+
+        slidesToShow: 3,
+        slidesToScroll: 1,
+
+        autoplay: true, 
+        autoplaySpeed: 10000,
+
+        responsive:[
+            {
+                breakpoint: 1000,
+                settings:{
+                    slidesToShow: 2,
+                    slidesToScroll: 2                   
+                }
+            },{
+                breakpoint: 680,
+                settings:{
+                    slidesToShow: 1,
+                    slidesToScroll: 1                   
+                }
+            }
+        ]
+
+      });
+});
+
+$(function(){
+    $('.sl-slider-gallery').slick({
+        arrows:true,
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        adaptiveHeight: true,
+        waitForAnimate: true,
+        //fade: true,
+
+      });
+});
+
+window.addEventListener("resize", function() {
+    if (window.innerWidth >= 1000) {
+      $('.sl-slider').slick('unslick');
+      sliderIsLive = false;
+    }
+    if (window.innerWidth < 1000) {
+        $('.sl-slider').slick({
+        arrows:true,
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        adaptiveHeight: true,
+        waitForAnimate: true,
+        //fade: true,
+
+        slidesToShow: 3,
+        slidesToScroll: 1,
+
+        responsive:[
+            {
+                breakpoint: 1000,
+                settings:{
+                    slidesToShow: 2,
+                    slidesToScroll: 2                   
+                }
+            },{
+                breakpoint: 680,
+                settings:{
+                    slidesToShow: 1,
+                    slidesToScroll: 1                   
+                }
+            }
+        ]
+
+      });
+    }
+  });
+  
